fa2884bb3216f2efa4d37c1290b0efdf910ab22b
James Aimonetti <james@2600hz.com>
2016-11-15 23:29:47 +0000

    add checker for mkdocs config

---
+++ check_build.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import yaml, os.path
+
+def parse_page(page):
+    "parse a page for existence"
+    if isinstance(page, dict):
+        for header, pages in page.items():
+            parse_page(pages)
+    elif isinstance(page, list):
+        for p in page:
+            parse_page(p)
+    else:
+        if ("index.md" != page) and (not os.path.isfile(page)):
+            print "page ", page, " is not valid"
+
+stream = open("doc/mkdocs/mkdocs.yml", 'r')
+mkdocs = yaml.load_all(stream)
+
+for doc in mkdocs:
+    for k,v in doc.items():
+        if "pages" == k:
+            parse_page(v)
